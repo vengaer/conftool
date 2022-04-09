1c4862183a791a17e4a8a6e863e525dce54ab5c3
Vilhelm <vilhelm.engstrom@tuta.io>
2022-04-09 10:50:32 +0200

    Improve formatting for listing Vecs

--- src/lib.rs
+++ src/lib.rs
@@ -60,10 +60,27 @@ pub enum Switch {
     No
 }
 
+fn format_vec<T>(f: &mut fmt::Formatter<'_>, prefix: Option<&str>, v: &Vec<T>) -> fmt::Result
+where
+    T: fmt::Display
+{
+    if let Some(prefix) = prefix {
+        write!(f, "{}", prefix)?;
+    }
+    if let Some((last, elems)) = v.split_last() {
+        for elem in elems {
+            write!(f, "{}, ", elem)?;
+        }
+        write!(f, "{}", last)?;
+    }
+    write!(f, "\n")?;
+    Ok(())
+}
+
 impl fmt::Display for ConfigEntry {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}:\n", self.name)?;
-        write!(f, "  depends: {:?}\n", self.depends)?;
+        format_vec(f, Some("  depends: "), &self.depends)?;
         let enttype = match &self.enttype {
             EntryType::Switch(_) => "switch",
             EntryType::String(_) => "string",
@@ -71,16 +88,14 @@ impl fmt::Display for ConfigEntry {
         };
 
         write!(f, "  type: {}\n", enttype)?;
-        let choices = match &self.choices {
-            Some(choices) => {
-                format!("{:?}", choices)
-            },
+        write!(f, "  choices: ")?;
+        match &self.choices {
+            Some(choices) => format_vec(f, None, &choices)?,
             None => match &self.enttype {
-                EntryType::Switch(_) => "y, n".to_string(),
-                _ => format!("Any {}", enttype)
+                EntryType::Switch(_) => write!(f, "y, n\n")?,
+                _ => write!(f, "Any {}\n", enttype)?
             }
         };
-        write!(f, "  choices: {}\n", choices)?;
         write!(f, "  help: {}", self.help)?;
         Ok(())
     }
